a0dbd3f75807c8f5d25784add3e3c175925e7df7
root <patiltejas24@gmail.com>
2016-09-06 16:02:05 -0400

    button to open another activity added

--- app/src/main/java/com/example/root/sudoku/MainActivity.java
+++ app/src/main/java/com/example/root/sudoku/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.root.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +16,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button about_button =(Button) findViewById(R.id.about_button);
+        about_button.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(view.getContext(),about.class);
+                startActivityForResult(intent,0);
+            }
+        }
+        );
     }
 }
